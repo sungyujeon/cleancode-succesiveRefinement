351ce7fa82dde46e194e4496f2c756b0ca24dec0
sungyujeon <sungyujeon91@gmail.com>
2022-03-01 16:26:43 +0900

    init: 3개 매개변수로 Args 질의
    - -l : boolean
    - -p : int
    - -d : string
    - 세 인수가 정상적으로 넘어오면 Args 인스턴스에 질의를 던지고,
      ArgsException이 발생하면 오류 메시지 출력하는 프로그램

--- src/main/java/com/objectmentor/ObjectmentorApplication.java
+++ src/main/java/com/objectmentor/ObjectmentorApplication.java
@@ -8,6 +8,16 @@ public class ObjectmentorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ObjectmentorApplication.class, args);
+
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (Exception e) {
+            System.out.print("Argument error: %s\n", e.errorMessage());
+        }
     }
 
 }
