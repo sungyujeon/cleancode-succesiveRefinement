6b9e058129bd66a8722d56d5a1c610a5e412455a
sungyujeon <sungyujeon91@gmail.com>
2022-03-01 15:31:55 +0900

    init

---
+++ src/main/java/com/objectmentor/ObjectmentorApplication.java
@@ -0,0 +1,13 @@
+package com.objectmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectmentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectmentorApplication.class, args);
+    }
+
+}
