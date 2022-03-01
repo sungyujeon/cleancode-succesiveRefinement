d11f2c2ec8e2b3d85722fee35805245d7a3d5349
sungyujeon <sungyujeon91@gmail.com>
2022-03-01 17:25:16 +0900

    feat: marshaler 클래스 추가

--- src/main/java/com/objectmentor/Args.java
+++ src/main/java/com/objectmentor/Args.java
@@ -201,4 +201,30 @@ public class Args {
     public boolean has(char arg) {
         return argsFound.contains(arg);
     }
+
+
+    private class ArgumentMarshaler {
+        private boolean booleanValue = false;
+
+        public void setBoolean(boolean value) {
+            booleanValue = value;
+        }
+
+        public boolean getBoolean() {
+            return booleanValue;
+        }
+    }
+
+    private class BooleanArgumentMarshaler extends ArgumentMarshaler {
+
+    }
+
+    private class StringArgumentMarshaler extends ArgumentMarshaler {
+
+    }
+
+    private class IntegerArgumentMarshaler extends ArgumentMarshaler {
+        
+    }
 }
+
